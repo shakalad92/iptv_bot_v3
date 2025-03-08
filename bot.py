949bdf958f7acd3433178529b0b9a55bdae8c17b
Arthur <arthur.petrosyan92@gmail.com>
2025-03-08 14:24:45 +0400

    modified pytest_args

--- bot.py
+++ bot.py
@@ -21,7 +21,8 @@ def generate_pytest_args(command_name: str, args: list) -> list:
         "ctest": ["--phone_number"]
     }
 
-    pytest_args = ['--headed', '-v', f'test_{command_name}.py']
+    # pytest_args = ['--headed', '-v', f'test_{command_name}.py']
+    pytest_args = ['--headed', '-v', '--chromium-arg="--no-sandbox,--disable-dev-shm-usage"', f'test_{command_name}.py']
     arg_keys = command_config.get(command_name)
 
     if not arg_keys:
